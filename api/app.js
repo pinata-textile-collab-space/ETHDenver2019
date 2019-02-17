4b24bfbcb16c81139d9ac75fe73e1c1cddbcae20
Matt Ober <mattober@MacBook-Pro-34.local>
2019-02-16 19:37:17 -0700

    updated env path

--- api/app.js
+++ api/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: './api/.env' })
+require('dotenv').config({ path: './.env' })
 const express = require('express');
 const ethers = require('ethers');
 const cors = require('cors');
